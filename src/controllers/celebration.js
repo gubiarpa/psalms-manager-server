9b10aec91392fc027f3e252278b332ab17496e2c
Giordan Arredondo <garredondop@uni.pe>
2022-06-14 15:22:04 -0500

    logs endpoints

--- src/controllers/celebration.js
+++ src/controllers/celebration.js
@@ -2,61 +2,86 @@ const { request, response } = require('express');
 const { Celebration } = require('../models');
 const { ObjectId } = require('mongoose').Types;
 const { celebrations } = require('../uploads/list')
+const logger = require('../utils/logger');
 
 const celebrationsGet = async(req = request, res = response) => {
-    const { from = 0, limit = 5 } = req.query;
-    const query = { state: true };
 
-    const [totalRows, celebrations] = await Promise.all([
-        Celebration.countDocuments(),
-        Celebration.find(query).skip(from).limit(limit)
-    ]);
+    try {
 
-    res.json({
-        totalRows,
-        celebrations
-    });
+        logger.info('Get list of celebrations');
+        logger.info('Execution of celebrationsGet');
+
+        const { from = 0, limit = 5 } = req.query;
+        const query = { state: true };
+        
+        const [totalRows, celebrations] = await Promise.all([
+            Celebration.countDocuments(),
+            Celebration.find(query).skip(from).limit(limit)
+        ]);
+        
+        res.json({
+            totalRows,
+            celebrations,
+        });
+
+    } catch (error) {
+        logger.error('Can not get list of celebrations');
+        logger.error(error);
+        res.status(400).send('Something broke!');
+    }
 }
 
 const celebrationPost = async(req = request, res = response) => {
 
-    const { name } = req.body;
-    const celebration = new Celebration({ name });
-    
-    const esMongoId = ObjectId.isValid( name );
-    if ( esMongoId ) {
-        const celebration = await Celebration.findById(name);
-        return res.json({
-            results: ( celebration ) ? [ celebration ] : []
-        })
-    }
-    
-    const regex = new RegExp( name, 'i' );
+    try {
 
-    const celebrations = await Celebration.find({
-        $or: [ 
-            { name: regex },  
-        ]
-    });
-
-    if (celebrations.length != 0) {
-        return res.json({
-            msg: 'Celebration already exists'
+        logger.info('Create a celebration');
+        logger.info('Execution of celebrationPost');
+        
+        const { name } = req.body;
+        const celebration = new Celebration({ name });
+        
+        const esMongoId = ObjectId.isValid( name );
+        if ( esMongoId ) {
+            const celebration = await Celebration.findById(name);
+            return res.json({
+                results: ( celebration ) ? [ celebration ] : []
+            })
+        }
+        
+        const regex = new RegExp( name, 'i' );
+    
+        const celebrations = await Celebration.find({
+            $or: [ 
+                { name: regex },  
+            ]
         });
+    
+        if (celebrations.length != 0) {
+            return res.json({
+                msg: 'Celebration already exists'
+            });
+        }
+    
+        await celebration.save();
+      
+        res.json({
+            msg: 'Celebration saved'
+        });
+    
+        // // for save all celebrations
+        // await Celebration.insertMany(celebrations);
+    
+        // res.json({
+        //     msg: 'Celebrations saved'
+        // });         
+
+    } catch (error) {
+        logger.error('Can not create a celebration');
+        logger.error(error);
+        res.status(400).send('Something broke!');
     }
 
-    await celebration.save();
-  
-    res.json({
-        msg: 'Celebration saved'
-    });
-
-    // // for save all celebrations
-    // await Celebration.insertMany(celebrations);
-
-    // res.json({
-    //     msg: 'Celebrations saved'
-    // });         
 
 }
 
--- src/controllers/psalm.js
+++ src/controllers/psalm.js
@@ -4,59 +4,82 @@ const { ObjectId } = require('mongoose').Types;
 const { songs } = require('../uploads/list')
 
 const psalmsGet = async(req = request, res = response) => {
-    const { from = 0, limit = 5 } = req.query;
-    const query = { state: true };
 
-    const [totalRows, psalms] = await Promise.all([
-        Psalm.countDocuments(),
-        Psalm.find().skip(from).limit(limit)
-    ]);
+    try {
 
-    res.json({
-        totalRows,
-        psalms
-    });
+        logger.info('Get list of psalms');
+        logger.info('Execution of psalmsGet');
+
+        const { from = 0, limit = 5 } = req.query;
+        const query = { state: true };
+    
+        const [totalRows, psalms] = await Promise.all([
+            Psalm.countDocuments(),
+            Psalm.find().skip(from).limit(limit)
+        ]);
+    
+        res.json({
+            totalRows,
+            psalms
+        });
+
+    } catch (error) {
+        logger.error('Can not get list of psalms');
+        logger.error(error);
+        res.status(400).send('Something broke!');
+    }
 }
 
 const psalmPost = async(req = request, res = response) => {
 
-    const { name } = req.body;
-    const psalm = new Psalm({ name });
-    
-    const esMongoId = ObjectId.isValid( name );
-    if ( esMongoId ) {
-        const psalm = await Psalm.findById(name);
-        return res.json({
-            results: ( psalm ) ? [ psalm ] : []
-        })
-    }
-    
-    const regex = new RegExp( name, 'i' );
+    try {
+        
+        logger.info('Create a psalm');
+        logger.info('Execution of psalmPost');
 
-    const psalms = await Psalm.find({
-        $or: [ 
-            { name: regex },  
-        ]
-    });
-
-    if (psalms.length != 0) {
-        return res.json({
-            msg: 'Psalm already exists'
+        const { name } = req.body;
+        const psalm = new Psalm({ name });
+        
+        const esMongoId = ObjectId.isValid( name );
+        if ( esMongoId ) {
+            const psalm = await Psalm.findById(name);
+            return res.json({
+                results: ( psalm ) ? [ psalm ] : []
+            })
+        }
+        
+        const regex = new RegExp( name, 'i' );
+    
+        const psalms = await Psalm.find({
+            $or: [ 
+                { name: regex },  
+            ]
         });
-    }
+    
+        if (psalms.length != 0) {
+            return res.json({
+                msg: 'Psalm already exists'
+            });
+        }
+    
+        await psalm.save();
+      
+        res.json({
+            msg: 'Psalm saved'
+        });
+    
+        // // for save all psalms
+        // await Psalm.insertMany(songs);
+    
+        // res.json({
+        //     msg: 'Psalms saved'
+        // });        
 
-    await psalm.save();
-  
-    res.json({
-        msg: 'Psalm saved'
-    });
-
-    // // for save all psalms
-    // await Psalm.insertMany(songs);
-
-    // res.json({
-    //     msg: 'Psalms saved'
-    // });        
+    } catch (error) {
+        logger.error('Can not create a psalm');
+        logger.error(error);
+        res.status(400).send('Something broke!');
+    }    
 
 }
 
