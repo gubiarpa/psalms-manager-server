9030eb91019c5065db1fb437b2e84fdd00288181
Giordan Arredondo <garredondop@uni.pe>
2022-06-14 15:22:16 -0500

    logs mongo db

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,14 @@
 const { createLogger, format, transports } = require('winston');
 
+require('winston-mongodb');
+require('dotenv').config();
+
 module.exports = createLogger({
     format: format.combine(
         format.simple(),
-        format.timestamp(),
-        format.printf(info => `[${info.timestamp}] ${info.level} ${info.message}`)
+        format.errors({ stack: true }),
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`),
     ),
     transports: [
         new transports.File({
@@ -14,6 +18,16 @@ module.exports = createLogger({
         }),
         new transports.Console({
             level: 'debug'
+        }),
+        new transports.MongoDB({
+            level: 'debug',
+            db: process.env.MONGO_CONNECTION,
+            options: { useUnifiedTopology: true },
+            collection: 'psalmLog',
+            format: format.combine( 
+                format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                format.json() 
+            )
         })
     ]
 });
